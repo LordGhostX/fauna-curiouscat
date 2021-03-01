e73d77bb95a3ba4418ea9cb62bb776fd5e5c2c2e
LordGhostX <solomonghost9@gmail.com>
2021-03-01 02:25:49 +0100

    added dashboard

--- app.py
+++ app.py
@@ -20,5 +20,10 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/dashboard/")
+def dashboard():
+    return render_template("dashboard.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
